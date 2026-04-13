1042ec7e07ca7ee77583db2570c92a7cee1d264b
José Alvarez Martinez <97672139+JoseAlvarezMartinez@users.noreply.github.com>
2026-04-13 09:14:30 -0300

    Error Toggle ToDo

--- js/app.js
+++ js/app.js
@@ -9,3 +9,52 @@ themeToggleBtn.addEventListener("click", () => {
     document.body.classList.toggle("light-theme");
     document.body.classList.toggle("dark-theme");
 })
+
+// Estado y Renderizado
+
+let todos = [];
+
+
+
+function renderTodos() {
+    toDoList.innerHTML = "";
+
+    todos.forEach(todo => {
+function toggleTodo(id) {
+            todos = todos.map(todo => {
+                todo.id === id ? { ...todo, completed: !todo.completed } : todo
+            })
+            renderTodos()
+        }
+
+
+        const li = document.createElement("li")
+        li.innerHTML = `
+            <input type="checkbox" onclick="toggleTodo(${todo.id})">
+            ${todo.text}
+            <button onclick="deleteTodo(${todo.id})">X</button>
+        `;
+
+        toDoList.appendChild(li)
+    })
+}
+
+// Escuchar evento submit del formulario
+
+toDoForm.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const text = toDoInput.value.trim()
+
+    if (text !== "") {
+        const newToDo = {
+            id: Date.now(),
+            text,
+            completed: false
+        }
+
+        todos.push(newToDo)
+        toDoInput.value = ""
+        renderTodos()
+    }
+})
